967b442fba4c90eed20037beb965d4d1e81650f0
bahailu-abera <aberabahailu4@gmail.com>
2022-03-15 14:30:22 -0700

    file added

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints the 24 hours
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 48; i < 51; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 53; k++)
+			{
+				for (l = 48; l <= 57; l++)
+				{
+					if (i >= 50 && j >= 52)
+						break;
+					_putchar(i);
+					_putchar(j);
+					_putcahr(58);
+					_putchar(k);
+					_putchar(l);
+					_putchar(10);
+				}
+			}
+		}
+	}
+}
