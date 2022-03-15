4cb736896643989ca2d9fd8506d7426ad9cb13f8
bahailu-abera <aberabahailu4@gmail.com>
2022-03-15 09:38:51 -0700

    comma at end

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,9 @@
  */
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114}
-	int count, size;
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count;
+	int size;
 
 	size = sizeof(str) / sizeof(int);
 	for (count = 0; count < size; count++)
