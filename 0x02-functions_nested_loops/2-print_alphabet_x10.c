bcc51c70f353d31f05f777d07752d39eba453b68
bahailu-abera <aberabahailu4@gmail.com>
2022-03-15 11:20:48 -0700

    file added'

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet in
+ * lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	int count;
+
+	while (i < 10)
+	{
+		for (count = 0; count <= 122; count++)
+		{
+			_putchar(count);
+		}
+		_putchar(10);
+	}
+}
