22ca07c573ad475bb991402e383bb19c166c3d7f
bahailu-abera <aberabahailu4@gmail.com>
2022-03-15 13:25:05 -0700

    bettty coding

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,14 +11,16 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n >= 0)
+	last_digit = n % 10;
+
+	if (last_digit < 0)
 	{
-		last_digit = n % 10;
+		_putchar(-last_digit + 48);
+		return (-last_digit);
 	}
 	else
 	{
-		last_digit = (-1 * n) % 10;
+		_putchar(last_digit + 48);
+		return (last_digit);
 	}
-	_putchar(last_digit + 48);
-	return (last_digit);
 }
