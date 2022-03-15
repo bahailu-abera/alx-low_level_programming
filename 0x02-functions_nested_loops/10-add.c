86e70aa82a8a326bb599cefcdd0e9e5a0a24cf65
bahailu-abera <aberabahailu4@gmail.com>
2022-03-15 14:47:38 -0700

    file added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - compute the sum of two numbers
+ *
+ * Return: The of the Integer
+ *
+ * @n: First number
+ * @m: second number
+ */
+int add(int n, int m)
+{
+	int sum = n + m;
+
+	return (sum);
+}
+
