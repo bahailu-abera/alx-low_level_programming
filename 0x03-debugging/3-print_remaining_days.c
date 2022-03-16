296ead3fdf619ffb9ae1a6af3f95498c8315ecb9
bahailu-abera <aberabahailu4@gmail.com>
2022-03-16 15:24:17 -0700

    algorithm change

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,9 +14,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
-		if (month > 2)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
@@ -27,8 +27,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Day of the year: %d\n", 59);
-			printf("Remaining days: %d\n", 365 - 59);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
