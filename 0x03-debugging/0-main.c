44a7b286eacc44f96bf8fa42fe664cbcaccd4a8b
bahailu-abera <aberabahailu4@gmail.com>
2022-03-16 13:07:50 -0700

    remove blank line

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - tests function that printsif integer is
+ * main - tests function that prints if integer is
  * positive or negative
- *
  * Return: 0
  */
 int main(void)
@@ -12,7 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-	putchar('\n');
 
 	return (0);
 }
