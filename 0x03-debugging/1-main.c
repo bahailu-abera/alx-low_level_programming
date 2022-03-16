28f4d537a7d6f3758c5b51d61b9e0d1e9ba36ab7
bahailu-abera <aberabahailu4@gmail.com>
2022-03-16 13:42:50 -0700

    from multiline comment to single line comment

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -11,12 +12,11 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-/**
-*	while (i < 10)
-*	{
-*		putchar(i);
-*	}
-*/
+
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
 
 	printf("Infinte loop avoided! \\o/\n");
 
