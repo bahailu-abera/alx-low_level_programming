c8eeef9ea30bd9b8d7f3f444ce594aacb00242c1
bahailu-abera <aberabahailu4@gmail.com>
2022-03-17 07:54:44 -0700

    output works

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,25 +13,17 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	if (n <= 0)
-		_putchar(10);
-	else
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		for (j = 0; j < i; j++)
 		{
-			for (j = 0; j < n; j++)
-			{
-				if (i == j)
-				{
-					_putchar(92);
-					
-				}
-				else if (j < i)
-				{
-					_putchar(32);
-				}
-			}
+			_putchar(32);
+		}
+		_putchar(92);
+		if (i < (n - 1))
+		{
 			_putchar(10);
 		}
 	}
+	_putchar(10);
 }
