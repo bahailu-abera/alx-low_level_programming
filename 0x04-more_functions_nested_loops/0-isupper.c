7978c297bfceab0bdb11739ec571f115294c2514
bahailu-abera <aberabahailu4@gmail.com>
2022-03-17 04:33:31 -0700

    file added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - returns 1 if the character is uppercase
+ *                 else 0 if the character is lowercase
+ * @c: character
+ * Return: 1(uppercase) or 0(lowercase)
+ */
+
+int _isupper(int c)
+{
+	if (c & 32)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
