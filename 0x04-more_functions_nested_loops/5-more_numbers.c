7166e4f25d24dd5f4b4512bf8b01b038c812f267
bahailu-abera <aberabahailu4@gmail.com>
2022-03-17 06:43:15 -0700

    only 3 _putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,20 +9,27 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
 	j = 0;
 
 	while (j < 10)
 	{
-		for (i = 48; i <= 57; i++)
+		for (i = 48; i < 50; i++)
 		{
-			_putchar(i);
-		}
-		for (i = 48; i <= 52; i++)
-		{
-			_putchar(49);
-			_putchar(i);
+			for (k = 48; k < 58; k++)
+			{
+				if (i == 49)
+				{
+					if (k == 53)
+					{
+						break;
+					}
+					else
+						_putchar(49);
+				}
+				_putchar(k);
+			}
 		}
 		_putchar(10);
 		j++;
