1d9a1963b5e305520db037e997a2407ce2d028f4
bahailu-abera <aberabahailu4@gmail.com>
2022-03-21 16:55:55 -0700

    correct output

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -17,8 +17,9 @@ void puts2(char *str)
 
 	while (*(str + count) != '\0')
 	{
-		_putchar(*(str + count));
-		count += 2;
+		if (count % 2 == 0)
+			_putchar(*(str + count));
+		count += 1;
 	}
 	_putchar(10);
 }
