74af031b24240c54449bc1640e22ba3e9f668735
bahailu-abera <aberabahailu4@gmail.com>
2022-03-23 13:52:53 -0700

    betty

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,29 +4,31 @@
  * _strcat - concatenates two strings
  * @dest: the destination of the string
  * @src: the source of the string
- * 
+ *
  * Return: String
  */
 
 char *_strcat(char *dest, char *src)
 {
-    int count;
-    int src_index;
+	int dest_index;
+	int src_index;
 
-    count = 0;
-    src_index = 0;
+	dest_index = 0;
+	src_index = 0;
 
-    while (*(dest + count) != '\0')
-    {
-        count++;
-    }
+	while (*(dest + dest_index) != '\0')
+	{
+		dest_index++;
+	}
 
-    while (*(src + src_index) != '\0')
-    {
-        *(dest + count) = *(src + src_index);
-        count++;
-        src_index++;
-    }
+	while (*(src + src_index) != '\0')
+	{
+		*(dest + dest_index) = *(src + src_index);
+		dest_index++;
+		src_index++;
+	}
+	*(dest + dest_index) = '\0';
 
-    return (dest);
+	return (dest);
 }
+
