99ed37900572fae897f14f3ed319a586f1c89e84
bahailu-abera <aberabahailu4@gmail.com>
2022-03-24 16:19:23 -0700

    header

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,9 @@ char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *s);
 void _putchar(char c);
+char *rot13(char *s);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 #endif /* MAIN>H */
 
