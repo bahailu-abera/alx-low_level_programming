963794e7af2d480afe16c66502d115d35e272108
bahailu-abera <aberabahailu4@gmail.com>
2022-03-24 13:48:37 -0700

    betty

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase
- *
+ * string_toupper -  changes all lowercase letters of a string to uppercase
  * @s: A string
  *
- * Return : lowercase string(s)
+ * Return: lowercase string(s)
  */
 
 char *string_toupper(char *s)
