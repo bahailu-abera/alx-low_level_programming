5601e5c89537522cfa93c903dbd631427efbfeca
bahailu-abera <aberabahailu4@gmail.com>
2022-03-24 13:00:28 -0700

    betty

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,19 +4,19 @@
  * reverse_array - reverse the content of an array
  * @a: the array
  * @n: number of array
- * 
+ *
  * Return: void
  */
 
 void reverse_array(int *a, int n)
 {
-    int i;
-    int temp;
+	int i;
+	int temp;
 
-    for (i = 0; i < n / 2; i++)
-    {
-        temp = a[i];
-        a[i] = a[n - (i + 1)];
-        a[n - (i + 1)] = temp;
-    }
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - (i + 1)];
+		a[n - (i + 1)] = temp;
+	}
 }
