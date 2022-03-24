69244952ed0686e0b6232c79d6912bab2f68328c
bahailu-abera <aberabahailu4@gmail.com>
2022-03-24 13:24:10 -0700

    file added

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ * @s: A string
+ *
+ * Return : void
+ */
+
+char *string_toupper(char *s)
+{
+	int index;
+
+	index = 0;
+
+	while (*(s + index) != '\0')
+	{
+		if ((s[index] >= 97) && (s[index] <= 122))
+		{
+			s[index] = s[index - 32];
+		}
+		index++;
+	}
+	return (s);
+}
