c8220ccd333f5743c8ae24b234c62ea62d00ab48
bahailu-abera <aberabahailu4@gmail.com>
2022-03-24 11:41:02 -0700

    file added

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: the holder of the string
+ * @src: The source of the string
+ * @n: length of the of string
+ *
+ * Return: string(dest)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int src_index;
+
+	src_index = 0;
+
+	while ((*(src + src_index) != '\0') && (src_index != n))
+	{
+		*(dest + src_index) = *(src + src_index);
+		src_index++;
+	}
+	*(dest + src_index) = '\0';
+
+	return (dest);
+}
