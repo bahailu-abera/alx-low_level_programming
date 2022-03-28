4fb1023d304a6c6589711e3fa95cbf549cd24946
bahailu-abera <aberabahailu4@gmail.com>
2022-03-28 15:09:59 -0700

    file added

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locate a character
+ *
+ * @s: the string
+ * @c: the character
+ *
+ * Return: the location of the character
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			return (&(*(s + i)));
+		}
+		i++;
+	}
+	return ('\0');
+}
