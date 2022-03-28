b776eaff29db4120ecde409249730b6b5a4dac40
bahailu-abera <aberabahailu4@gmail.com>
2022-03-28 15:44:16 -0700

    indetation

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,36 +10,36 @@
  */
 
 
- char *_strpbrk(char *s, char *accept)
- {
-	 unsigned int i = 0;
-	 unsigned int j;
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int j;
 
-	 while (*(s + i) != '\0')
-	 {
-		 while (*(accept + j) != '\0')
-		 {
-			 if (*(s + i) == *(accept + j))
-			 {
-				 return (s + i);
-			 }
-			 j++;
-		 }
-		 if (*(s + i) == *(accept + j))
-		 {
-			 return (s + i);
-		 }
-		 i++;
-	 }
-	 j = 0;
+	while (*(s + i) != '\0')
+	{
+		while (*(accept + j) != '\0')
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				return (s + i);
+			}
+			j++;
+		}
+		if (*(s + i) == *(accept + j))
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	j = 0;
 
-	 while (*(accept + j) != '\0')
-	 {
-		 if (*(s + i) == *(accept + j))
-		 {
-			 return (s + i);
-		 }
-		 j++;
-	 }
-	 return ('\0');
- }
+	while (*(accept + j) != '\0')
+	{
+		if (*(s + i) == *(accept + j))
+		{
+			return (s + i);
+		}
+		j++;
+	}
+	return ('\0');
+}
