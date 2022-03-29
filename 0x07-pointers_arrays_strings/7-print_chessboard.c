1385a7dc948c36196fa6658d3572a9ddcf1e8f85
bahailu-abera <aberabahailu4@gmail.com>
2022-03-28 17:04:35 -0700

    output

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,14 +10,16 @@
 
 void print_chessboard(char (*a)[8]);
 {
-	unsigned int i, j;
+	unsigned int i, j = 0;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 64; i++)
 	{
-		for (j = 0; j < 8; j++)
+		if (i % 8 == 0 && i != 0)
 		{
-			_putchar(a[i][j]);
+			j = 1;
+			_putchar('\n');
 		}
+		_putchar(a[i / 8][i - j]);
 	}
-	_putchar(10);
+	_putchar('\n');
 }
