29bb231993762612a44f1a426a1e1ff636a70389
bahailu-abera <aberabahailu4@gmail.com>
2022-03-30 06:42:01 -0700

    made it recursive

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,13 +11,19 @@
 
 void _puts_recursion(char *s)
 {
+	char *s2;
 	unsigned int i = 0;
 
+	if (*s == '\0')
+		_putchar(10);
+	else
+		_putchar(s[0]);
 	while (*(s + i) != '\0')
 	{
-		_putchar(*(s + i));
+		*(s2 + i) = *(s + i + 1);
 		i++;
 	}
-	_putchar(10);
+	*(s2 + i) = '\0';
+	return (_puts_recursion(s2));
 }
 
