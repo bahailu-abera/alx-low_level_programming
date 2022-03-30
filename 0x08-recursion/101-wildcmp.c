cdab9bbcb696233a4d2e5cfda2c8d9b0d82210e6
bahailu-abera <aberabahailu4@gmail.com>
2022-03-30 16:08:34 -0700

    edge case

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,7 +2,7 @@
 #include "2-strlen_recursion.c"
 
 /**
- * comparator - compare two strings
+ * compar - compare two strings
  * @s1: the first string
  * @s2: the second string
  * @last_s1: last index of s1
@@ -10,9 +10,9 @@
  *
  * Return: 1 (identical) otherwise 0
  */
-int comparator(char *s1, char *s2, int last_s1, int last_s2)
+int compar(char *s1, char *s2, int last_s1, int last_s2)
 {
-	if (*(s2 + last_s2) == '*')
+	if (*(s2 + last_s2) == '*' && (last_s2 - 1 == 0 || *(s2 + last_s2) != '.'))
 		return (1);
 	if (last_s2 == 0 && last_s1 != 0)
 		return (0);
@@ -20,7 +20,7 @@ int comparator(char *s1, char *s2, int last_s1, int last_s2)
 		return (1);
 	if (*(s1 + last_s1) != *(s2 + last_s2))
 		return (0);
-	return (comparator(s1, s2, last_s1 - 1, last_s2 - 1));
+	return (compar(s1, s2, last_s1 - 1, last_s2 - 1));
 }
 
 /**
@@ -35,5 +35,5 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	return (comparator(s1, s2, _strlen_recursion(s1) - 1, _strlen_recursion(s2) - 1));
+	return (compar(s1, s2, _strlen_recursion(s1) - 1, _strlen_recursion(s2) - 1));
 }
