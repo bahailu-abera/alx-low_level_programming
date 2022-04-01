e202c15c1e0671a3809db0a2d4a875bb9afaa936
bahailu-abera <aberabahailu4@gmail.com>
2022-04-01 12:25:45 -0700

    type correction

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 /**
  * main - add two numbers
  *
@@ -13,21 +13,22 @@
 int main(int argc, char **argv)
 {
 	int sum = 0;
-	int i;
+	int i, j;
 
 	if (argc <= 1)
 		printf("%d\n", 0);
 	for (i = 1; i < argc; i++)
 	{
-		if (argv[i] >= 48 && argv[i] <= 57)
-			sum += atoi(argv[i]);
-		else
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			break;
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
+		sum += atoi(argv[i]);
 	}
-	if (i == argc)
-		printf("%d\n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
