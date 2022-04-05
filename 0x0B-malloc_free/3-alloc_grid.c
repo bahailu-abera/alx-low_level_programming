1570d841140394da10632e5029d39db3eef08b78
bahailu-abera <aberabahailu4@gmail.com>
2022-04-05 14:59:36 -0700

    free memory when failure

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,13 +22,23 @@ int **alloc_grid(int width, int height)
 	arr = (int **)malloc(sizeof(int *) * height);
 
 	if (arr == NULL)
+	{
+		free(arr);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *)(malloc(sizeof(int) * width));
 		if (arr[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+			{
+				free(arr[i]);
+			}
 			return (NULL);
+			free(arr);
+		}
 	}
 	for (i = 0; i < height; i++)
 	{
