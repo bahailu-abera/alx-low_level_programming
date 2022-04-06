da1282d3aa90389c90610a9e998b355a747594e7
bahailu-abera <aberabahailu4@gmail.com>
2022-04-06 16:18:14 -0700

    betty code

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,64 +3,64 @@
 
 /**
  * strtow - splits a string into words.
- * @str:the string 
+ * @str:the string
  * Return: a pointer to a string
  */
 char **strtow(char *str)
 {
-    int i, j, k = 0, count, ar = 0;
-    char *word;
-    char **arr;
+	int i, j, k = 0, count, ar = 0;
+	char *word;
+	char *arr;
 
-    for (i = j = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] != 32)
-        {
-            j++;
-            if (str[i + 1] == 32 || str[i + 1] == '\0')
-            {
-                j++;
-                k++;
-                j = 0;
-            }
-        }
-    }
-    arr = (char **)malloc(sizeof(char *) * (k + 1));
-    if (arr == NULL)
-           return (NULL);
-    for (i = j = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] != 32)
-        {
-            if (j == 0)
-                count = i;
-            j++;
-            if (str[i + 1] == 32 || str[i + 1] == '\0')
-            {
-                j++;
-                word = malloc(sizeof(char) * j);
-                if (word == NULL)
-                {
-                    for (; ar >= 0; ar--)
-                    {
-                        free(arr[ar]);
-                    }
-                    free(arr);
-                    return (NULL);
-                }
-                for (k = 0; k < j; k++)
-                {
-                    word[k] = *(str + count);
-                    count++;
-                }
-                word[k] = '\0';
-                arr[ar] = word;
-                ar++;
-                j = 0;
-                k = 0;
-            }
-        }
-    }
-    arr[ar] = NULL;
-    return (arr);
+	for (i = j = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != 32)
+		{
+			j++;
+			if (str[i + 1] == 32 || str[i + 1] == '\0')
+			{
+				j++;
+				k++;
+				j = 0;
+			}
+		}
+	}
+	arr = (char **)malloc(sizeof(char *) * (k + 1));
+	if (arr == NULL)
+		return (NULL);
+	for (i = j = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != 32)
+		{
+			if (j == 0)
+				count = i;
+			j++;
+			if (str[i + 1] == 32 || str[i + 1] == '\0')
+			{
+				j++;
+				word = malloc(sizeof(char) * j);
+				if (word == NULL)
+				{
+					for (; ar >= 0; ar--)
+					{
+						free(arr[ar]);
+					}
+					free(arr);
+					return (NULL);
+				}
+				for (k = 0; k < j; k++)
+				{
+					word[k] = *(str + count);
+					count++;
+				}
+				word[k] = '\0';
+				arr[ar] = word;
+				ar++;
+				j = 0;
+				k = 0;
+			}
+		}
+	}
+	arr[ar] = NULL;
+	return (arr);
 }
