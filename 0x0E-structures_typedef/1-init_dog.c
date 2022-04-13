4f9dc0b61584dfa7a3fe901ff8af2da3c0b36d46
bahailu-abera <aberabahailu4@gmail.com>
2022-04-12 23:42:52 -0700

    intialize the dog struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+/**
+ * init_dog - initializes instance of dog
+ * @d: struct d
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: No return value
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
