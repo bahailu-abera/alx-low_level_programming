1a8c5e544b6e866fd6edb3efc579b488a12e5f9a
bahailu-abera <aberabahailu4@gmail.com>
2022-04-13 01:18:30 -0700

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include "stdlib.h"
+
+/**
+ * free_dog - frees the memory hold
+ * by the struct dog
+ * @d: the struct dog
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
