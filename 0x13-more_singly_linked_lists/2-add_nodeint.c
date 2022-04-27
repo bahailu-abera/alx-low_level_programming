9de25e3184ceed66933bb0395fe0536b4f36dfa1
bahailu-abera <aberabahailu4@gmail.com>
2022-04-27 14:33:37 -0700

    add at begginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,7 +10,7 @@
  * Return: the head of the linked list
  */
 
-list_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
 
