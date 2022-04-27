67fc70fa592cef275a72472756ffe6c046279dea
bahailu-abera <aberabahailu4@gmail.com>
2022-04-27 04:56:39 -0700

    add node at the beggining

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the
+ * beggining of a list_t
+ *
+ * @head: address of head
+ * @str: a pointer to the string of the new node
+ *
+ * Return: the head of the linked list
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
+}
