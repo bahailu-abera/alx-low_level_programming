57bec8c60c804fc9db3374ad893e310083ae9c2f
bahailu-abera <aberabahailu4@gmail.com>
2022-04-28 04:42:36 -0700

    handle index 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 			new_node->n = n;
 			new_node->next = curr;
-			prev->next = new_node;
+			if (count == 0)
+				*head = new_node;
+			else
+				prev->next = new_node;
 
 			return (new_node);
 		}
