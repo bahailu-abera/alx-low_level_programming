c95bba4512a3f37229a51e3401a0d149dacb30e1
bahailu-abera <aberabahailu4@gmail.com>
2022-04-28 05:00:46 -0700

    handle null

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 
-	if (count == idx)
+	if (curr != NULL && count == idx)
 	{
 		new_node->n = n;
 		new_node->next = curr;
