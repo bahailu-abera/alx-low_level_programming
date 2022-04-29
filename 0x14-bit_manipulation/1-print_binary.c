6e3a924a527bc94bffabcfa78c8d6eb374128349
bahailu-abera <aberabahailu4@gmail.com>
2022-04-29 10:38:47 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * print_binary - prints the binary
+ * representation of unsigned integer
+ *
+ * @n: the integer representation in decimal
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long  int n)
+{
+	int bit_pos = 0;
+	unsigned long int temp;
+
+	while ((temp > 1) && (++bit_pos))
+		temp >>= 1;
+
+	while (bit_pos >= 0)
+		printf("%d", ((n >> bit_pos) & 1));
+	printf("\n");
+}
