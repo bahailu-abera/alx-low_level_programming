d5fa2f597316e611242c552dc867c115ede22d19
bahailu abera <aberabahailu4@gmail.com>
2022-04-30 20:22:20 +0300

    Update 9-main.c
    test with loop

--- 0x13-more_singly_linked_lists/9-main.c
+++ 0x13-more_singly_linked_lists/9-main.c
@@ -13,7 +13,6 @@ int main(void)
     listint_t *head;
     listint_t *head2;
     listint_t *node;
-
     head2 = NULL;
     add_nodeint(&head2, 0);
     add_nodeint(&head2, 1);
@@ -24,6 +23,16 @@ int main(void)
     add_nodeint(&head2, 402);
     add_nodeint(&head2, 1024);
     print_listint_safe(head2);
+    head = NULL;
+    node = add_nodeint(&head, 0);
+    add_nodeint(&head, 1);
+    add_nodeint(&head, 2);
+    add_nodeint(&head, 3);
+    add_nodeint(&head, 4);
+    node->next = add_nodeint(&head, 98);
+    add_nodeint(&head, 402);
+    add_nodeint(&head, 1024);
+    print_listint_safe(head);
 
     return (0);
 }
