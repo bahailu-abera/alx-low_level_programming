437bfa3d45b79e8c672fb1f2ee4555c43fbb13f3
bahailu-abera <aberabahailu4@gmail.com>
2022-05-05 08:49:25 -0700

    handle null

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,8 +23,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	if (text_content == NULL && fd < 0)
-		return (-1);
+	if (text_content == NULL)
+		return (1);
 
 	for (len = 0; *(text_content + len) != '\0'; len++)
 		;
