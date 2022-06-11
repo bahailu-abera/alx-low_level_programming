f9665816088c7450bfd504e717e32d6002bb1138
bahailu <aberabahailu4@gmail.com>
2022-06-11 19:43:38 +0300

    length of dlist

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - a function that determines the length of
+ * doubly linked list
+ * @h: the header of the dlist.
+ *
+ * Return: the length of the dlist
+ */
+size_t dlistint_len(const dlistint_s *h)
+{
+	size_t len = 0;
+
+	for (; h != NULL; h = h->next, len++)
+		;
+	return (len);
+}
