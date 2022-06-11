6d83c26eb84330930bbf96b04a3920cff40a959e
bahailu <aberabahailu4@gmail.com>
2022-06-11 20:01:26 +0300

    naming error fixed

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,18 +1,20 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds new node at the beginning of a dlist_t.
+ * add_dnodeint - adds new node at the beginning of a dlist_t.
  * @head: pointer to the head of the dlist.
  * @n: the value of the new node.
  *
  * Return: the address of the new node.
  * on failure it returns NULL.
  */
-dlistint_t *add_nodeint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new;
+
 	if (head == NULL)
 		return (NULL);
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
 		return (NULL);
