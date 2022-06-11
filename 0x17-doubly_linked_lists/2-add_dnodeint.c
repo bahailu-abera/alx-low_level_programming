4fbbcb2f9ca95a707f0500f3c8be61d7f0145a02
bahailu <aberabahailu4@gmail.com>
2022-06-11 20:07:13 +0300

    handle null

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,7 +21,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
-	*head = new;
+
+	if (*head == NULL)
+		*head = new;
+	else
+		(*head)->prev = new, *head = new;
 
 	return (new);
 }
