a451844bd77ebc535d8c5fd6978a2e348e068dab
bahailu <aberabahailu4@gmail.com>
2022-07-04 16:30:11 +0300

    pointer to hash_node_t

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (hash_table == NULL)
 		return (NULL);
 
-	array = malloc(sizeof(hash_node_t) * size);
+	array = malloc(sizeof(hash_node_t *) * size);
 	if (array == NULL)
 		return (NULL);
 
