a3732f17d7576ca14561058fa2d0e3f83d107dee
bahailu <aberabahailu4@gmail.com>
2022-07-06 19:38:57 +0300

    sorted hash table proto

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -68,6 +68,7 @@ typedef struct shash_table_s
      shash_node_t *stail;
 } shash_table_t;
 
+/** hash table manipulaton functions prototype **/
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -75,6 +76,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
+/** sorted hash table manipulation functions prototype **/
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
+
 
 
 #endif
