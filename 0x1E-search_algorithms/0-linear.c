bf8f40fde66340ce23eaeebaa25c90b3573a4882
bahailu <aberabahailu4@gmail.com>
2022-10-03 09:10:09 +0300

    interactive

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * linear_search - searches for a value in an array of integers
  *
@@ -14,6 +16,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
