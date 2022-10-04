463065b7f135aaa577aa610b886d504a7372a04f
bahailu <aberabahailu4@gmail.com>
2022-10-04 11:15:57 +0300

    handle null prop

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -14,7 +14,7 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	size_t l = 0, h = size - 1, pos;
 
-	if (array == NULL || *array == '\0')
+	if (array == NULL)
 		return (-1);
 
 	while ((value >= array[l] && value <= array[h]))
