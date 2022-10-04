90a7b634c4ef54a5b1741df92735c1f0d0ec750e
bahailu <aberabahailu4@gmail.com>
2022-10-04 08:26:10 +0300

    handle NULL

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,6 +14,8 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t root = sqrt(size), low = 0, high = 0;
 
+	if (array == NULL)
+		return (-1);
 	while (high < size && array[high] < value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", high, array[high]);
