0bcdfa50d0f57c89e5038f857f6e97fd10cc3259
bahailu <aberabahailu4@gmail.com>
2022-10-26 10:09:07 +0300

    handle all case

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,23 +8,16 @@ def island_perimeter(grid):
     """
     Returns the perimeter of the island described in grid
     """
-    first_row = None
-    last_row = None
-    first_col = None
-    last_col = None
+    perimeter = 0
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if first_row is None:
-                    first_row = i
-                if first_col is None or first_col > j:
-                    first_col = j
-                if last_col is None or last_col < j:
-                    last_col = j
-                last_row = i
-    if first_row is None:
-        return (0)
-    perimeter = 2 * (last_col - first_col + last_row - first_row + 2)
+                if j == 0 or grid[i][j - 1] != 1:
+                    perimeter += 4
+                else:
+                    perimeter += 2
+                if i != 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
 
     return perimeter
